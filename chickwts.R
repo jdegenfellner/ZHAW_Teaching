8ce0aa7ba1d9e501b257fa27379c0808f21a1627
jdegenfellner <jdegenfellner@gmail.com>
2023-02-07 09:48:37 +0100

    up

--- chickwts.R
+++ chickwts.R
@@ -1,12 +1,10 @@
 library(tidyverse)
 library(flextable)
 library(gtsummary)
-#library(pillar)
+
 
 str(chickwts)
-
 tot_mean <- mean(chickwts$weight)
-
 options(digits = 7)
 
 # Slide 11, LM2.pdf
@@ -20,11 +18,12 @@ boxplot(weight ~ feed, data = chickwts)
 # Slide 12, LM2.pdf
 chickwts %>% ggplot(aes(x = feed, y = weight)) +
   geom_boxplot() + 
-  geom_jitter(size = 0.4) + # show points
+  geom_jitter(size = 0.4) + # Show points
   ggtitle("Boxplot with ggplot") + 
-  coord_flip() + # rotate 90 degrees, looks nice
-  theme_dark() +  # change theme for fun, looks even nicer
-  theme(plot.title = element_text(hjust = 0.5)) # center title
+  coord_flip() + # Rotate 90 degrees, looks nice
+  theme_dark() +  # Change theme for fun, looks even nicer
+  theme(plot.title = element_text(hjust = 0.5)) + # Center title
+  geom_hline(yintercept = tot_mean, color = "red") # Add red line indicating the total mean
 
 # Side 13, LM2.pdf
 mod <- lm(weight ~ feed, data = chickwts)
