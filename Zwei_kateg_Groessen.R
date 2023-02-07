4fada665389cdbb5396f05c090b180b213d09455
jdegenfellner <jdegenfellner@gmail.com>
2023-02-07 12:14:37 +0100

    up

--- Zwei_kateg_Groessen.R
+++ Zwei_kateg_Groessen.R
@@ -20,19 +20,31 @@ d.cat2 <- data.frame(response, age, therapy)
 str(d.cat2)
 
 mod <- lm(response ~ therapy*age, data = d.cat2) # "*" test main AND interaction effects, ":" is only the interaction effect
-#summary(mod)
+# from help: The * operator denotes factor crossing: a*b is interpreted as a + b + a:b.
+summary(mod)
+
+# What about R^2?
+library(semEff)
+R2(mod, adj.type = "olkin-pratt", pred = FALSE)[2] # Unbiased estimator
+summary(mod)$r.squared
+summary(mod)$adj.r.squared
+
 tbl_regression(mod)
 coef(mod)
 predict(mod, newdata = data.frame(therapy = "Trt", age = "old")) # 69.34672
-# check manually:
+# check manually (just plug in):
+34.8891800 + 6.3137427 + 26.8931452 + 1.2506546 # 69.34672
 
 
-
-
-
-#mod_int_only <- lm(response ~ therapy:age, data = d.cat2) # "*" test main AND interaction effects, ":" is only the interaction effect
-#summary(mod_int_only)
-#tbl_regression(mod_int_only)
-
-str(d.cat2)
-
+# What happens here?
+mod_int_only <- lm(response ~ therapy:age, data = d.cat2)
+summary(mod_int_only)
+tbl_regression(mod_int_only, intercept = TRUE)
+predict(mod_int_only, newdata = data.frame(therapy = "Ctrl", age = "child")) # 34.88918 
+# manually:
+69.347 - 34.458 # 34.889, identical
+# It seems that just a normal regression is estimated in the respective sub-groups
+# Let's verify this:
+d.cat2_sub1 <- d.cat2 %>% filter(therapy == "Ctrl", age == "child")
+mod_sub1 <- lm(response ~ 1, data = d.cat2_sub1)
+summary(mod_sub1) # Estimate = 34.889, This is just the mean in the subgroup!
