486e931af5d073e415d4648eba54416507f46241
jdegenfellner <jdegenfellner@gmail.com>
2023-02-07 09:28:12 +0100

    up

--- chickwts.R
+++ chickwts.R
@@ -7,9 +7,18 @@ tot_mean <- mean(chickwts$weight)
 
 options(digits = 7)
 
-# Slide 11, LM 2
+# Slide 11, LM2.pdf
 chickwts %>% group_by(feed) %>% 
   summarise(Durchschnitt = mean(weight),
             Effekt = mean(weight) - tot_mean)
 
 boxplot(weight ~ feed, data = chickwts)
+
+# Slide 12, LM2.pdf
+chickwts %>% ggplot(aes(x = feed, y = weight)) +
+  geom_boxplot() + 
+  geom_jitter(size = 0.4) + # show points
+  ggtitle("Boxplot with ggplot") + 
+  theme(plot.title = element_text(hjust = 0.5)) # center title
+  
+                    
