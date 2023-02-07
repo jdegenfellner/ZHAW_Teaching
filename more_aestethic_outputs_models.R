44f3d348e4ec7253bf8407559898db042ab757e5
jdegenfellner <jdegenfellner@gmail.com>
2023-02-07 09:41:18 +0100

    up

--- more_aestethic_outputs_models.R
+++ more_aestethic_outputs_models.R
@@ -1,6 +1,6 @@
 library(report)
 
-mod <- lm(Fertility ~ ., data = swiss)
+mod <- lm(Fertility ~ ., data = swiss) # Explain Fertility by ALL other variables in the data set
 report(mod)
 report_performance(mod)
 report_parameters(mod)
@@ -14,6 +14,6 @@ check_model(mod)
 # in progress
 
 library(flextable) # more beautiful tables
-library(gtsummary)
+library(gtsummary) # show regression results nicely
 
 tbl_regression(mod) # create nice table for output
