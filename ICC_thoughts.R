a9fc23b11447e699ac802bdebad4ebb4fefcd9a9
jdegenfellner <jdegenfellner@gmail.com>
2023-02-09 09:06:58 +0100

    up

--- ICC_thoughts.R
+++ ICC_thoughts.R
@@ -1,6 +1,6 @@
 # ICC, some thoughts:
-
-n <- 1:20
+seq(0.1, 1, by = 0.1)
+n <- c(seq(0.1, 1, by = 0.1), 1:20)
 sigma_e <- 1
 sigma_p <- sigma_e/n
 
@@ -8,4 +8,8 @@ ICC <- sigma_p/(sigma_p + sigma_e)
 
 df <- data.frame(sigma_p = sigma_p, proportion = n, ICC = ICC)
 df %>% ggplot(aes(x = proportion, y = ICC)) + 
-  geom_line() + xlab("sigma_p*n = sigma_e")
+  geom_line() + xlab("n: sigma_p = sigma_e/n") + 
+  ggtitle("Relationship between error-variances and ICC") + 
+  theme(plot.title = element_text(hjust = 0.5)) + 
+  geom_vline(xintercept = 1, color = "red") + 
+  annotate("text", x = 1.7, y = 0.1, label = "n=1", color = "red")
