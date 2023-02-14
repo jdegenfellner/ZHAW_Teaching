d19f5ab5a98024090ae9380ec6fe50ad958c1ef6
jdegenfellner <jdegenfellner@gmail.com>
2023-02-14 09:22:02 +0100

    up

---
+++ Influence_of_outlieres_on_t-Test.R
@@ -0,0 +1,12 @@
+# Consider the t-Test
+gr1 <- rnorm(40, mean = 0, sd = 1)
+gr2 <- rnorm(40, mean = 1, sd = 1)
+t.test(gr1, gr2)
+
+# Introduce an outlier
+gr2[3] <- gr2[3] + 100
+mean(gr2)
+sd(gr2)
+
+# What happens to the p-Value and why?
+t.test(gr1, gr2)$p.value
