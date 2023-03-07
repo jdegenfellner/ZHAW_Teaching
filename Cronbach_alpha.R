9bf228b529b3a91181c02fe28a766d65b02db535
jdegenfellner <jdegenfellner@gmail.com>
2023-03-07 09:06:01 +0100

    up

--- Cronbach_alpha.R
+++ Cronbach_alpha.R
@@ -1,19 +1,22 @@
 # Cronbach's Alpha, tau-äquivalente Reliabilität
 
-# in progresss
+# in progresss # 
 
 library(ltm)
 
 #enter survey responses as a data frame
-data <- data.frame(Q1=c(1, 2, 2, 3, 2, 2, 3, 3, 2, 3),
-                   Q2=c(1, 1, 1, 2, 3, 3, 2, 3, 3, 3),
-                   Q3=c(1, 1, 2, 1, 2, 3, 3, 3, 2, 3))
+data <- data.frame(Q1 = c(1, 2, 2, 3, 2, 2, 3, 3, 2, 3),
+                   Q2 = c(1, 1, 1, 2, 3, 3, 2, 3, 3, 3),
+                   Q3 = c(1, 1, 2, 1, 2, 3, 3, 3, 2, 3))
 
 #calculate Cronbach's Alpha
 cronbach.alpha(data)
 
-Cronbach's alpha for the 'data' data-set
 
-Items: 3
-Sample units: 10
-alpha: 0.773
+# Calculate manually:
+# https://de.wikipedia.org/wiki/Cronbachsches_Alpha
+
+# Formula 1:
+# N*r_bar/(1+(N-1)*r_bar)
+
+N <- dim(data)[1]
