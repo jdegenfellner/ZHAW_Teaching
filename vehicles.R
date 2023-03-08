799c5dbe18308034ed5b4abdf90c07353f96dd2b
jdegenfellner <jdegenfellner@gmail.com>
2023-03-08 12:39:56 +0100

    up

--- vehicles.R
+++ vehicles.R
@@ -1,7 +1,12 @@
 library(data.table)
 library(tidyverse)
+library(DataExplorer)
 
 # Set working directory to source file location
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 vehicles <- read.csv("./Data/Electric_Vehicle_Population_Data.csv")
+create_report(vehicles)
+describe(vehicles)
+
+pie(table(vehicles$Make))
