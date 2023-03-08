384305def8edf5426a3fc0e7797d45c7138c90e4
jdegenfellner <jdegenfellner@gmail.com>
2023-03-08 11:40:16 +0100

    up

--- injury_statistics.R
+++ injury_statistics.R
@@ -5,3 +5,23 @@ library(tidyverse)
 # Set working directory to source file location
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+inj <- read.csv("./Data/serious-injury-outcome-indicators-2000-2020-CSV.csv",
+                header = TRUE,
+                sep = ",",
+                dec = ".")
+
+View(inj)
+str(inj)
+
+# Prepare-----------------------------------------------------------------------
+inj$Period_posix <- as.POSIXct(strptime(inj$Period, "%Y-%m"))
+# Problem: two different data formats in one variable!
+
+unique(nchar(inj$Period)) # 4/7; two formats
+
+# -> simplify to year only
+inj$Period <- ifelse(nchar(inj$Period) == 7,substr(inj$Period,1,4), inj$Period)
+unique(nchar(inj$Period)) # 4
+
+
+
