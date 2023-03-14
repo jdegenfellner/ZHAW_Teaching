12aa35a3584570534887bc63294eebf3cfad9e2b
jdegenfellner <jdegenfellner@gmail.com>
2023-03-14 16:52:33 +0100

    up

--- Sample_size_calculation_basic.R
+++ Sample_size_calculation_basic.R
@@ -92,21 +92,40 @@ df %>% filter(effect_sizes >= 0.2) %>%
   theme(plot.title = element_text(hjust = 0.5))
 
 
-# 3) Sample size for a multiple linear regression model-------------------------
+# 3) Sample size calculation for a multiple linear regression model-------------
 
 # Let's pretend we know the true model
-n <- 15
+n <- 100
 x1 <- rnorm(n = n, mean = 4, sd = 2)
-x2 <- rnorm(n = n, mean = 7, sd = 4)
-y <- 2*x1 - 3*x2 + rnorm(n = n, mean = 0, sd = 10) # add relatively heavy noise
+x2 <- rnorm(n = n, mean = 7, sd = 2.5)
+y <- 2*x1 - 2.7*x2 + rnorm(n = n, mean = 0, sd = 10) # Add relatively strong noise
 df <- data.frame(x1 = x1, x2 = x2, y = y)
-
 mod <- lm(y ~ x1 + x2, data = df)
 summary(mod)
+confint(mod)
+#plot(mod$residuals)
+
+mod_x1 <- lm(y ~ x1, data = df)
+summary(mod_x1)
+mod_x2 <- lm(y ~ x2, data = df)
+summary(mod_x2)
 
 # find out power for 
-Rsquared <- 0.40
+Rsquared <- 0.90
 
 library(pwr)
-f_2 <- Rsquared/(1 - Rsquared)
-pwr.f2.test(u = 2-1, v = n - 2 - 1, f2 = f_2, sig.level = 0.05, power = NULL)
+
+# Cohens f2:
+# see e.g. https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3328081/
+
+# Cohens f^2 for global (!) effect size
+(f_2 <- Rsquared/(1 - Rsquared))
+
+pwr.f2.test(u = 2, v = n - 2 - 1, f2 = f_2, sig.level = 0.05, power = NULL) # degrees of freedom see also summary() output!
+
+# Cohens f^2 for: x2 explains a percentage more than x1 alone:
+# ...
+(f_2_x2 <- )
+
+# Check via simulation----
+
