b1600272a153a0572240c842bd63ad507c9e808f
jdegenfellner <jdegenfellner@gmail.com>
2023-03-14 14:26:55 +0100

    up

---
+++ Sample_size_calculation_basic.R
@@ -0,0 +1,5 @@
+# Sample size calculations # 
+# Some thoughts and basic examples # 
+
+# 1) QM1 Script, p. 176
+power.t.test(delta = 0.5, sd = 1, power = 0.8, type = "one.sample", alternative = "one.sided")
