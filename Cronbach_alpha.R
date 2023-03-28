17be688fae91fe115ca29f7f5cffc3bd45b9661b
jdegenfellner <jdegenfellner@gmail.com>
2023-03-28 07:32:27 +0200

    up

--- Cronbach_alpha.R
+++ Cronbach_alpha.R
@@ -4,12 +4,12 @@
 
 library(ltm)
 
-#enter survey responses as a data frame
+# Enter survey responses as a data frame
 data <- data.frame(Q1 = c(1, 2, 2, 3, 2, 2, 3, 3, 2, 3),
                    Q2 = c(1, 1, 1, 2, 3, 3, 2, 3, 3, 3),
                    Q3 = c(1, 1, 2, 1, 2, 3, 3, 3, 2, 3))
 
-#calculate Cronbach's Alpha
+# Calculate Cronbach's Alpha
 cronbach.alpha(data) # 0.773
 
 
