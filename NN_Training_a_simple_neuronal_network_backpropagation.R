86e545897219fef2e5135ee886d35af2de9ec8ea
jdegenfellner <jdegenfellner@gmail.com>
2023-05-12 12:18:01 +0200

    up

--- NN_Training_a_simple_neuronal_network_backpropagation.R
+++ NN_Training_a_simple_neuronal_network_backpropagation.R
@@ -2,3 +2,10 @@
 
 # in progress # 
 
+# (Initial) instruction to GPT-4:
+# "I want to explain backpropagation training to my students. I want to 
+# present a nice R script showing step by step the training process for a 
+# small, simple neural network.
+# So, let's use a data set that is included in R or in one of it's popular 
+# packages and do that. Maybe you can also visualize the updated weights 
+# in a step by step manner as the training data comes in."
