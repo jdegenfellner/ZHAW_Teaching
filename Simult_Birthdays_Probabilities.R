c61200c8727a91f81e2a732537466005e3abad0b
jdegenfellner <jdegenfellner@gmail.com>
2023-11-14 07:51:40 +0100

    up

--- Simult_Birthdays_Probabilities.R
+++ Simult_Birthdays_Probabilities.R
@@ -1,7 +1,16 @@
-#GebProbs.R
+# Probabilites of simultaneous birthdays
+
+# Set working directory to source file location:
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
+if (!require(pacman)) {
+  install.packages("pacman")
+  library(pacman)
+}
+p_load(tidyverse)
 
 n <- 10:50
-probs_exact <- round(1-choose(365,n)*factorial(n)/365^n,digits=4)
+probs_exact <- round(1-choose(365,n)*factorial(n)/365^n,digits=4) # exact probabilities (Exercises Problem 9)
 probs_exact
 probs_approx <- 1-exp(-(n^2/(2*365))) # Approximation heuristically by my colleague Thomas.
 
@@ -9,10 +18,11 @@ df <- data.frame(n = n, ExactProbs = probs_exact, ApproxProbs = probs_approx)
 
 df %>% ggplot(aes(x=n)) + 
   geom_line(aes(x=n, y=probs_approx, colour = "approx")) + 
-  geom_line(aes(x=n, y=probs_exact, colour = "exact"))
+  geom_line(aes(x=n, y=probs_exact, colour = "exact")) + 
+  theme(legend.title = element_blank())
 
 options(digits = 6)
-max(probs_approx-probs_exact)
+max(probs_approx-probs_exact) # maximum error
 #0.0123693
 
 # Determine parameter(s) using (non-linear) regression
@@ -28,4 +38,4 @@ ggplot(df, aes(x = n)) +
   theme_minimal()
 
 max(df$PredictedProbs - df$ExactProbs)
-#0.00989698
+#0.00989698; a little improvement 
