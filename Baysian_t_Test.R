2db2fc727e8c1786241947e2cf4e23fb3a236a15
jdegenfellner <jdegenfellner@gmail.com>
2023-11-20 15:21:05 +0100

    up

--- Baysian_t_Test.R
+++ Baysian_t_Test.R
@@ -1,16 +1,16 @@
-library(report)
-library(Bolstad)
-#devtools::install_github("rasmusab/bayesian_first_aid")
+library(pacman)
+p_load(report, Bolstad, BayesianFirstAid)
+#devtools::install_github("rasmusab/bayesian_first_aid") # if not installed
 
 # Ex 1.:
 bayes.t.test(1:10, y = c(7:20)) 
 
 # Ex 2.:
 ?sleep
+str(sleep)
 bayes.t.test(extra ~ group, data = sleep)
 
 
-library(BayesianFirstAid)
 # Based on Kruschke, 2012:
 # https://www.rdocumentation.org/packages/BayesianFirstAid/versions/0.1/topics/bayes.t.test
 
