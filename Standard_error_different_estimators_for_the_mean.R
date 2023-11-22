63fa01a298af169b1e046c84b5bd3e38f797fa05
jdegenfellner <jdegenfellner@gmail.com>
2023-11-22 09:37:37 +0100

    up

--- Standard_error_different_estimators_for_the_mean.R
+++ Standard_error_different_estimators_for_the_mean.R
@@ -7,7 +7,7 @@ p_load(tidyverse)
 # Set working directory to source file location
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-# Generiere eine sample aus einer Zufallszahl (wir wissen hier, wie diese
+# Generiere eine sample aus einer Zufallszahl (wir wissen hier, wie diese----
 # verteilt ist):
 x <- rnorm(10000, mean = 23.4, sd = 5.6)
 df <- data.frame(values = x)
@@ -22,3 +22,57 @@ p2 <- ggplot(df, aes(x = values)) +
   ggtitle("Histogram with density plot and boxplot below") +
   theme(plot.title = element_text(hjust = 0.5))
 p2
+
+# Wir wollen den wahren Mittelwert 23.4 schaetzen:----
+
+# Schaetzer 1: arithmetisches Mittel
+(x1 <- mean(x))
+abs(x1-23.4) # z.B. 0.0551746
+
+# Schaetzer 2: Mittelwert aus groesster und kleinster Beobachtung
+(x2 <- mean(c(min(x), max(x))))
+abs(x2-23.4) # z.B. 0.5289857
+
+
+# Das machen wir jetzt 1000 mal, damit wir einen Eindruck davon bekommen,
+# wie gut die beiden Schaetzer sind.
+
+# Set the seed for reproducibility
+set.seed(0)
+
+# Function to perform the estimation process
+estimate_means <- function() {
+  x <- rnorm(10000, mean = 23.4, sd = 5.6)
+  x1 <- mean(x)
+  x2 <- mean(c(min(x), max(x)))
+  return(c(x1, x2))
+}
+
+# Perform the process 1000 times and store the results
+estimates <- replicate(1000, estimate_means())
+estimates_df <- as.data.frame(t(estimates))
+colnames(estimates_df) <- c("arithm_Mittel", "Mittelwert_Min_Max")
+
+# Standardfehler:
+(std_dev_x1 <- sd(estimates_df$arithm_Mittel))
+(std_dev_x2 <- sd(estimates_df$Mittelwert_Min_Max))
+
+# Convert the data frame to long format using pivot_longer from the tidyr package
+estimates_long <- pivot_longer(estimates_df, cols = everything(), names_to = "Estimator", values_to = "Estimate")
+
+# Plotting the histograms using ggplot2
+ggplot(estimates_long, aes(x = Estimate, fill = Estimator)) +
+  geom_histogram(aes(y = after_stat(density)), position = 'identity', alpha = 0.7, bins = 30) +
+  facet_wrap(~Estimator, scales = 'free_y') +
+  theme_minimal() +
+  labs(title = "", 
+       x = "Estimated Value", 
+       y = "Density") + 
+  theme(plot.title = element_text(hjust = 0.5)) + 
+  annotate("text", x = Inf, y = Inf, label = sprintf("Std.fehler arithm.Mittel: %0.2f", std_dev_x1),
+           hjust = 1.1, vjust = 2, size = 3.5, color = "red") +
+  annotate("text", x = Inf, y = Inf, label = sprintf("Std.fehler MinMax: %0.2f", std_dev_x2),
+           hjust = 1.1, vjust = 1, size = 3.5, color = "blue")
+
+
+
