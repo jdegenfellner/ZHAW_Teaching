eea34447c0897150992dd7cded7e8d07c345cd3f
jdegenfellner <jdegenfellner@gmail.com>
2023-11-27 09:50:04 +0100

    up

--- Estimate_coverage_prob_mean_from_CLT.R
+++ Estimate_coverage_prob_mean_from_CLT.R
@@ -28,10 +28,11 @@ estimate_coverage_probability <- function(distr, params,
 }
 
 
-# Example:
+# Example:----
 estimate_coverage_probability("rnorm", list(mean = 0, sd = 1), sample_size = 10)
 
-# Look at different sample sizes:
+# Look at different sample sizes:----
+# _1) Standard normal N(0,1)----
 sizes <- seq(from = 10, to = 200, by = 2)
 df <- data.frame(sizes = sizes, 
                  cov_prob = sizes)
@@ -44,8 +45,50 @@ for(i in 1:length(sizes)){
 df %>% ggplot(aes(x = sizes, y = cov_prob)) + 
   geom_line() + 
   geom_smooth() +
-  geom_hline(yintercept = 0.95) + 
+  geom_hline(yintercept = 0.95, linetype = "dashed", color = "red") + 
   ggtitle("(Simulated) Coverage probability of the CI for the mean (from CLT)") + 
   theme(plot.title = element_text(hjust = 0.5)) + 
   ylab("Simulated coverage probability") + xlab("sample size (n)")
   
+
+# _2) Gamma----
+estimate_coverage_probability_gamma <- function(distr, params, sample_size, num_simulations = 10000, conf_level = 0.95) {
+  coverage_count <- 0
+  
+  for (i in 1:num_simulations) {
+    sample <- do.call(distr, c(list(n = sample_size), params))
+    sample_mean <- mean(sample)
+    sample_se <- sd(sample) / sqrt(sample_size)
+    z <- qnorm(1 - (1 - conf_level) / 2)
+    ci_lower <- sample_mean - z * sample_se
+    ci_upper <- sample_mean + z * sample_se
+    true_mean <- params$shape / params$rate
+    if (ci_lower <= true_mean && ci_upper >= true_mean) {
+      coverage_count <- coverage_count + 1
+    }
+  }
+  
+  coverage_probability <- coverage_count / num_simulations
+  return(coverage_probability)
+}
+
+# Now try the function with the gamma distribution
+estimate_coverage_probability_gamma("rgamma", list(shape = 2, rate = 1), sample_size = 10)
+
+
+for(i in 1:length(sizes)){
+  df$cov_prob[i] <- estimate_coverage_probability_gamma("rgamma", list(shape = 2, rate = 1), 
+                                                  sample_size = sizes[i])
+}
+
+df %>% ggplot(aes(x = sizes, y = cov_prob)) + 
+  geom_line() + 
+  geom_smooth() +
+  geom_hline(yintercept = 0.95, linetype = "dashed", color = "red") + 
+  ggtitle("(Simulated) Coverage probability of the CI for the mean (from CLT)") + 
+  theme(plot.title = element_text(hjust = 0.5)) + 
+  ylab("Simulated coverage probability") + xlab("Sample size (n)")
+
+
+# TODO
+# use only one function for all distributions
