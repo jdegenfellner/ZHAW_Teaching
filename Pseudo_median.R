1b7bda2f4cd4fdcd2b004dd934b48e8c175657b0
jdegenfellner <jdegenfellner@gmail.com>
2023-12-11 13:25:45 +0100

    up

--- Pseudo_median.R
+++ Pseudo_median.R
@@ -6,21 +6,23 @@
 # https://stats.stackexchange.com/questions/335892/what-is-pseudomedian-in-r-function-wilcox-test
 # https://github.com/SurajGupta/r-source/blob/master/src/library/stats/R/wilcox.test.R
 
-# Example
+# Example----
 x <- rnorm(4000)
 y <- rnorm(4000, mean = 0.5)
 res <- wilcox.test(x, y, paired = TRUE, conf.int = TRUE)
 #wilcox.test(x - y, conf.int = TRUE)
 res$estimate
+# compare to:
+median(x - y) # since symmetric
 
-# Reproduce?:
-# Hodges-Lehmann estimator for paired data
+# Reproduce?:----
+# _Hodges-Lehmann estimator for paired data----
 diffs <- x - y
-average_diffs <- (outer(diffs, diffs, "+") / 2)[lower.tri(diffs)]
+average_diffs <- (outer(diffs, diffs, "+") / 2)[lower.tri(diffs)] # includes 0-diffs in the diagonal
 median(average_diffs) # close
 median(average_diffs) - res$estimate
 
-# in wilcox.test()-code (we should be in the one-sample case for x-y): 
+# _in wilcox.test()-code (we should be in the one-sample case for x-y): ----
 diffs <- outer(x-y, x-y, "+")
 diffs <- sort(diffs[!lower.tri(diffs)]) / 2
 # ESTIMATE = 
