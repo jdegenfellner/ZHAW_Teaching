ad018c34d55d0598b02b7f37501b9a0b7eb73acf
jdegenfellner <jdegenfellner@gmail.com>
2023-12-12 08:00:05 +0100

    up

--- Pseudo_median.R
+++ Pseudo_median.R
@@ -7,20 +7,17 @@
 # https://github.com/SurajGupta/r-source/blob/master/src/library/stats/R/wilcox.test.R
 
 # Example----
-x <- rnorm(4000)
-y <- rnorm(4000, mean = 0.5) # ...maybe choose skewed data
+#x <- rnorm(4000)
+#y <- rnorm(4000, mean = 0.5) # ...maybe choose skewed data
+
+x <- c(13.22, 6.81, 10.22, 14.03, 8.04, 10.16, 9.43, 13.07, 13.63, 5.05, 11.63)
+y <- c(15.44, 6.69, 11.89, 16.25, 9.27, 10.74, 10.67, 13.52, 14.13, 7.21, 14.79)
+
 res <- wilcox.test(x, y, paired = TRUE, conf.int = TRUE)
 #wilcox.test(x - y, conf.int = TRUE)
 res$estimate
 # compare to:
-median(x - y) # very similar if symmetric
-
-# Reproduce?:----
-# _Hodges-Lehmann estimator for paired data----
-diffs <- x - y
-average_diffs <- (outer(diffs, diffs, "+") / 2)[lower.tri(diffs)] # includes 0-diffs in the diagonal
-median(average_diffs) # close
-median(average_diffs) - res$estimate
+median(x - y) # should be very similar if symmetric
 
 # _in wilcox.test()-code (we should be in the one-sample case for x-y): ----
 diffs <- outer(x-y, x-y, "+")
