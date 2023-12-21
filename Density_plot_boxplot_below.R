ccfa272198f7cc28ff7a9237ad6b3cf072924d98
jdegenfellner <jdegenfellner@gmail.com>
2023-12-21 10:52:51 +0100

    up

--- Density_plot_boxplot_below.R
+++ Density_plot_boxplot_below.R
@@ -1,5 +1,7 @@
 # Density plot with boxplot below
 
+# From: https://github.com/jdegenfellner/ZHAW_Teaching/blob/main/Density_plot_boxplot_below.R
+
 library(pacman)
 p_load(tidyverse)
 
@@ -13,7 +15,7 @@ df <- data.frame(values = x)
 
 p2 <- ggplot(df, aes(x = values)) +
   geom_histogram(aes(y = after_stat(density)), bins = 30, alpha = 0.7, color="darkgrey") +
-  geom_density(aes(y = after_stat(density)), color = "blue", size=1) +
+  geom_density(aes(y = after_stat(density)), color = "blue", linewidth=1) +  # Verwenden Sie hier `linewidth` anstelle von `size`
   geom_boxplot(aes(y = -0.01, x = values), width = 0.02, position = position_nudge(y = -0.00)) +
   geom_point(aes(y = -0.01), 
              position = position_jitter(width = 0.002, height = 0.01), 
@@ -21,3 +23,4 @@ p2 <- ggplot(df, aes(x = values)) +
   ggtitle("Histogram with density plot and boxplot below") +
   theme(plot.title = element_text(hjust = 0.5))
 p2
+# Ihr weiterer R-Code...
