5ca3438ef73f780e50bd5766d018e57b12a537bf
jdegenfellner <jdegenfellner@gmail.com>
2024-01-24 13:16:50 +0100

    up

---
+++ Simple_Linear_Regression.R
@@ -0,0 +1,6 @@
+# QM2 FS24
+# Juergen Degenfellner
+
+# https://raw.githubusercontent.com/jdegenfellner/ZHAW_Teaching/main/Data/regressionSimple.csv
+# https://raw.githubusercontent.com/jdegenfellner/ZHAW_Teaching/main/Data/regression.csv
+
