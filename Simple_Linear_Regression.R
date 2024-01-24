b700c60f8c21a0281ec744d21c6ad7017aac02d7
jdegenfellner <jdegenfellner@gmail.com>
2024-01-24 14:53:28 +0100

    up

--- Simple_Linear_Regression.R
+++ Simple_Linear_Regression.R
@@ -18,3 +18,25 @@ df <- read.csv("https://raw.githubusercontent.com/jdegenfellner/ZHAW_Teaching/ma
 str(df)
 mod <- lm(y ~ x, data = df) # least squares method.
 summary(mod)
+
+sse <- function(alpha, beta, data) {
+  predicted <- alpha + beta * data$x
+  sum((data$y - predicted)^2)
+}
+
+alpha_values <- seq(-5, 5, length.out = 100)
+beta_values <- seq(-5, 5, length.out = 100)
+sse_values <- outer(alpha_values, beta_values, Vectorize(function(a, b) sse(a, b, df)))
+fig <- plot_ly(x = ~alpha_values, y = ~beta_values, z = ~sse_values, type = "surface")
+fig
+
+# Highlight min
+min_sse_index <- which(sse_values == min(sse_values), arr.ind = TRUE)
+min_alpha <- alpha_values[min_sse_index[1]]
+min_beta <- beta_values[min_sse_index[2]]
+min_sse <- min(sse_values)
+
+fig <- fig %>% add_markers(x = ~min_alpha, y = ~min_beta, z = ~min_sse, 
+                           marker = list(color = 'red', size = 10))
+fig
+
