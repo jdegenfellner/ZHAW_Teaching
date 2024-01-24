2df7a352774c370edb798488601e9bdf398f9eaa
jdegenfellner <jdegenfellner@gmail.com>
2024-01-24 13:19:02 +0100

    up

--- Simple_Linear_Regression.R
+++ Simple_Linear_Regression.R
@@ -1,6 +1,7 @@
 # Some code for simple linear regression
 # 24.1.24
 # QM2 
+# See also Script QM2 (Meichtry, S.2-13)
 
 # From https://github.com/jdegenfellner/ZHAW_Teaching/blob/main/Simple_Linear_Regression.R
 
