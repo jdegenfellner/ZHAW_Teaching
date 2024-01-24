6cb31e5928b3f6a9248a8fb588d7bdbf591d32c2
jdegenfellner <jdegenfellner@gmail.com>
2024-01-24 14:48:51 +0100

    up

--- Simple_Linear_Regression.R
+++ Simple_Linear_Regression.R
@@ -1,13 +1,20 @@
 # Some code for simple linear regression
+# From https://github.com/jdegenfellner/ZHAW_Teaching/blob/main/Simple_Linear_Regression.R
 # 24.1.24
 # QM2 
 # See also Script QM2 (Meichtry, S.2-13)
 # See also https://github.com/jdegenfellner/ZHAW_Teaching/blob/main/Simple_linear_regression_shiny.R
 
-# From https://github.com/jdegenfellner/ZHAW_Teaching/blob/main/Simple_Linear_Regression.R
-
-
 # Example data for simple linear regression----
 # https://raw.githubusercontent.com/jdegenfellner/ZHAW_Teaching/main/Data/regressionSimple.csv
 # https://raw.githubusercontent.com/jdegenfellner/ZHAW_Teaching/main/Data/regression.csv
 
+library(pacman)
+p_load(plotly, tidyverse)
+
+# ad page 5 Script ----
+# Let's look at formula (1.2.6) and use data from above
+df <- read.csv("https://raw.githubusercontent.com/jdegenfellner/ZHAW_Teaching/main/Data/regressionSimple.csv")
+str(df)
+mod <- lm(y ~ x, data = df) # least squares method.
+summary(mod)
