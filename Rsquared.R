b25b925362940761dc9c8352d142877c444b9b7d
jdegenfellner <jdegenfellner@gmail.com>
2024-02-22 07:27:46 +0100

    up

--- Rsquared.R
+++ Rsquared.R
@@ -1,16 +1,23 @@
 # Bestimmtheitsmass, wie groß ist dieses, wenn man nur Rauschen hat?
 
-n <- 1000
-r_squared <- rep(NA, 1000)
+library(tictoc)
 
-plot(rnorm(1000), rnorm(1000))
+# Set working directory to source file location
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+n <- 5000
+r_squared <- rep(NA, n)
+
+plot(rnorm(n), rnorm(n))
+
+tic()
 for(i in 1:n){
-  x <- rnorm(1000)
-  y <- rnorm(1000)
+  x <- rnorm(n)
+  y <- rnorm(n)
   df <- data.frame(x = x, y = y)
   s <- summary(lm(y ~ x, data = df))
   r_squared[i] <- s$r.squared
 }
+toc()
 
 hist(r_squared)
