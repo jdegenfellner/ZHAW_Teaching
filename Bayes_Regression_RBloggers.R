537fcd4c9d92167d6a4e817f4f88b4312b53d32d
jdegenfellner <jdegenfellner@gmail.com>
2024-03-06 21:50:26 +0100

    up

--- Bayes_Regression_RBloggers.R
+++ Bayes_Regression_RBloggers.R
@@ -10,7 +10,8 @@ p_load(mlbench,
        bayesplot,
        insight,
        broom,
-       tidyverse)
+       tidyverse,
+       flextable)
 
 data("BostonHousing")
 str(BostonHousing)
@@ -66,7 +67,6 @@ mcmc_dens(model_bayes, pars=c("dis"))+
 # the posteriors using some specific statistics. To get the full statistics 
 # provided by bayestestR package, we make use of the function describe_posterior
 
-library(flextable)
 flextable(describe_posterior(model_bayes, 
                              ci_method = "HDI", # Highest Density Interval (HDI), All points within this interval have a higher probability density than points outside the interval. The HDI can be used in the context of uncertainty characterisation of posterior distributions as Credible Interval (CI).
                              rope_range = "default", # = sd(bost$medv)*0.1 ... If "default", the bounds are set to x +- 0.1*SD(response)
