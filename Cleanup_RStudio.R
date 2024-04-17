560420fc3c90f3abaa40f88c687961a033f632c8
jdegenfellner <jdegenfellner@gmail.com>
2024-04-17 16:50:15 +0200

    up

--- Cleanup_RStudio.R
+++ Cleanup_RStudio.R
@@ -1,10 +1,10 @@
-# Cleanup.R
-
-# Clear Console
-cat("\014")
+# from: https://github.com/jdegenfellner/ZHAW_Teaching/blob/main/Cleanup_RStudio.R
 
 # Remove all objects from the workspace
 rm(list = ls())
 
 # Clear all plots
 graphics.off()
+
+# Clear Console
+cat("\014")
