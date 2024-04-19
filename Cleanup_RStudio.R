5863d08d4c057e9bfbdae7fcfc8f0915cbe3e315
jdegenfellner <jdegenfellner@gmail.com>
2024-04-19 10:16:44 +0200

    up

--- Cleanup_RStudio.R
+++ Cleanup_RStudio.R
@@ -6,5 +6,15 @@ rm(list = ls())
 # Clear all plots
 graphics.off()
 
+clear_viewer_pane <- function() {
+  dir <- tempfile()
+  dir.create(dir)
+  TextFile <- file.path(dir, "blank.html")
+  writeLines("", con = TextFile)
+  rstudioapi::viewer(TextFile) 
+}
+
+clear_viewer_pane()
+
 # Clear Console
 cat("\014")
