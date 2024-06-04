20f3ee1dd5a28083ba0efc9d008666af308dc782
jdegenfellner <jdegenfellner@gmail.com>
2024-06-04 07:38:02 +0200

    up

--- NN_Euclidean_distances_in_high_dimenstions.R
+++ NN_Euclidean_distances_in_high_dimenstions.R
@@ -1,8 +1,18 @@
 # NN (Euclidean) distances in high dimensions:
 
-n <- 10 # dimensions
+n <- 6000 # dimensions
+num_sim <- 10000 # number of draws
 
 # Let's draw random vectors from the unit cube [0,1]^n
 
+length_of_unit_vector <- norm(rep(1,n), type="2")
+
 distances <- rep(0, n)
-for
+for(i in 1:num_sim){
+  x1 <- runif(n)
+  x2 <- runif(n)
+  distances[i] <- norm(x1 - x2, type = "2")
+}
+
+hist(distances)
+
