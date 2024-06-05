ddffdc1e523630d269444fcdd41e0be4a377dcbe
jdegenfellner <jdegenfellner@gmail.com>
2024-06-05 13:15:09 +0200

    up

---
+++ Maximum_Likelihood_Estimation_coin_flip_Logit_plot.R
@@ -0,0 +1,24 @@
+# GLM - simple example of maximum likelihood (ML) estimation using coin flip
+
+theta <- seq(from=0.01, to = 0.99, by = 0.001)
+theta
+
+likelihood_f <- theta^9*(1-theta)^8 # 9 heads, 8 tails
+
+plot(theta, likelihood_f)
+
+which.max(likelihood_f)
+
+theta[520]
+
+plot(theta, log(likelihood_f))
+
+log_likelihood <- log(likelihood_f)
+
+which.max(log_likelihood)
+
+#---------
+
+p <- seq(from=0.000001, to = 0.9999999099, by = 0.001)
+p/(1-p) # odds
+plot(p, log(p/(1-p)))
