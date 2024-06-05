94bbe1386293f9ff0be02433871eb8f463312582
jdegenfellner <jdegenfellner@gmail.com>
2024-06-05 18:12:44 +0200

    up

--- Maximum_Likelihood_Estimation_coin_flip_Logit_plot.R
+++ Maximum_Likelihood_Estimation_coin_flip_Logit_plot.R
@@ -3,15 +3,26 @@
 theta <- seq(from=0.01, to = 0.99, by = 0.001)
 theta
 
-likelihood_f <- theta^9*(1-theta)^8 # 9 heads, 8 tails
+likelihood_f <- theta^6*(1-theta)^14 # 9 heads, 8 tails
 
 plot(theta, likelihood_f)
 
 which.max(likelihood_f)
 
-theta[520]
+theta[which.max(likelihood_f)] # 0.3
 
-plot(theta, log(likelihood_f))
+data.frame(theta = theta, log_likelihood = log(likelihood_f)) %>%
+ggplot(aes(x = theta, y = log_likelihood)) +
+  geom_line() +
+  geom_vline(xintercept = 0.3, color = "red", linetype = "dashed") +
+  labs(x = "theta", y = "log(likelihood_f)", title = "Log-Likelihood") +
+  scale_x_continuous(breaks = seq(0, 1, by = 0.1)) + 
+  theme_minimal() + 
+  theme(plot.title = element_text(hjust = 0.5),
+        axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size = 14),
+        axis.text.x = element_text(size = 12),
+        axis.text.y = element_text(size = 12))
 
 log_likelihood <- log(likelihood_f)
 
