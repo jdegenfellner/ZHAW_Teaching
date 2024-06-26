5e20b5e0aaeef3c97ac50370b599ad2bb37bff80
jdegenfellner <jdegenfellner@gmail.com>
2024-06-26 13:32:16 +0200

    up

--- Overestimating_effects_when_significant.R
+++ Overestimating_effects_when_significant.R
@@ -22,13 +22,35 @@ for(i in 1:n_sim){
   
   mod <- lm(y ~ x1 + x2, data = df)
   
-  coef_x1[i] <- coef(mod)[1]
-  coef_x2[i] <- coef(mod)[2]
+  coef_x1[i] <- coef(mod)[2]
+  coef_x2[i] <- coef(mod)[3]
   
   summary_mod <- summary(mod)
   
-  p_value_x1[i] <- summary_mod$coefficients["x1", "Pr(>|t|)"]
-  p_value_x2[i] <- summary_mod$coefficients["x2", "Pr(>|t|)"]
+  p_val_x1[i] <- summary_mod$coefficients["x1", "Pr(>|t|)"]
+  p_val_x2[i] <- summary_mod$coefficients["x2", "Pr(>|t|)"]
 }
 
+df_res <- data.frame(p_val_x1 = p_val_x1,
+                     p_val_x2 = p_val_x2,
+                     coef_x1 = coef_x1,
+                     coef_x2 = coef_x2)
 
+df_res %>% filter(p_val_x1 < 0.05) %>%
+  ggplot(aes(x = coef_x1)) + 
+  geom_histogram() + 
+  geom_vline(xintercept = beta_x1) # looks fine!
+
+df_res %>% filter(p_val_x2 < 0.05) %>%
+  ggplot(aes(x = coef_x2)) + 
+  geom_histogram() + 
+  geom_vline(xintercept = beta_x2) # rather skewed.
+
+df_res %>% 
+  filter(p_val_x2 < 0.05) %>%
+  dplyr::summarize(mean_coef_x2 = mean(coef_x2, na.rm = TRUE))
+# overestimated systematically
+
+
+mean(coef_x1) # very good
+mean(coef_x2) # very good
