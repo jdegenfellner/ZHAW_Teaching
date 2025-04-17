934b27bcba7ed0cc5db8e7659aa6241754489a0b
jdegenfellner <jdegenfellner@gmail.com>
2025-04-17 10:58:56 +0200

    up

---
+++ Prisoner_execution_riddle.R
@@ -0,0 +1,28 @@
+# "Three prisoners on death row are told that one of them has been chosen 
+# at random for execution the following morning, but the other two are to 
+# be freed. One privately begs the warden to at least tell him the name of 
+# one other prisoner who will be freed, and the warden relents. “Susie will 
+# go free,” he says. Suddenly horrified, the first prisoner says that because
+# he is now one of only two remaining prisoners at risk, his chances of 
+# execution have risen from one-third to one-half!"
+
+
+set.seed(42)
+n <- 1e5
+executed <- sample(c("A", "B", "C"), n, replace = TRUE) # 1/3 wahrsch für A,B,C
+
+# Für alle Fälle mit A als Fragender:
+named <- character(n)
+
+for (i in 1:n) {
+  prisoners <- c("A", "B", "C")
+  other <- c("B", "C")
+  free_others <- setdiff(other, executed[i])
+  named[i] <- sample(free_others, 1)  # Wärter wählt zufällig eine der sicheren Freilassungen
+}
+
+# Fälle, in denen Wärter B genannt hat
+table(executed[named == "B"]) / sum(named == "B")
+
+# Fälle, in denen Wärter C genannt hat
+table(executed[named == "C"]) / sum(named == "C")
