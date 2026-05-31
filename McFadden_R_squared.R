27d99ef7cdf1501af3f5e230b9dbaaf75cdab813
Jürgen Degenfellner <jdegenfellner@gmail.com>
2026-05-31 11:38:59 +0200

    up

--- McFadden_R_squared.R
+++ McFadden_R_squared.R
@@ -1,4 +1,33 @@
-library(tidyverse)
+library(pacman)
+p_load(
+  tidyverse,
+  performance,
+  pscl
+)
+
+# ln(L) and ln(L0) are both negative because the likelihood
+# is a product of Bernoulli probabilities, each of which lies
+# between 0 and 1. Consequently, the likelihood itself is also
+# between 0 and 1, and its logarithm is therefore negative.
+#
+# See:
+# https://web.stanford.edu/class/archive/cs/cs109/cs109.1178/
+# lectureHandouts/220-logistic-regression.pdf
+#
+# As model fit improves, ln(L) becomes less negative and moves
+# closer to zero. Therefore, the ratio
+#
+#   ln(L) / ln(L0)
+#
+# approaches 1 because the numerator and denominator become
+# more similar in magnitude. McFadden's R² is defined as
+#
+#   R² = 1 - ln(L) / ln(L0)
+#
+# and therefore increases as the fitted model improves relative
+# to the null model. McFadden's R² equals 0 when the fitted
+# model is no better than the null model and approaches 1 as
+# model fit improves substantially.
 
 set.seed(123)
 
@@ -18,7 +47,10 @@ mcfadden_r2 <- function(model_full, model_null) {
   ll_null <- as.numeric(stats::logLik(model_null))
   
   1 - ll_full / ll_null
+  # see start of file...
 }
+
+
 
 run_simulation <- function(beta1_values, n = 1000, beta0 = 0) {
   
@@ -103,3 +135,41 @@ plot_logistic_curve <- function(beta0 = 0, beta1 = 1, n = 1000) {
 }
 
 plot_logistic_curve(beta0 = 0, beta1 = 30)
+
+# check mcfadden with different packages:----------
+
+sim_dat <- simulate_data(
+  n = 1000,
+  beta0 = 0,
+  beta1 = 3
+)
+
+model_full <- glm(
+  y ~ x,
+  data = sim_dat,
+  family = binomial()
+)
+
+model_null <- glm(
+  y ~ 1,
+  data = sim_dat,
+  family = binomial()
+)
+
+manual_mcfadden <- mcfadden_r2(
+  model_full,
+  model_null
+)
+
+performance_mcfadden <- performance::r2_mcfadden(
+  model_full
+)
+
+pscl_mcfadden <- pscl::pR2(
+  model_full
+)["McFadden"]
+
+manual_mcfadden
+performance_mcfadden
+pscl_mcfadden
+# all identical.
